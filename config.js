69ad5d2864279a58b1b98177bda7fcac58614928
Harun Sokullu <harunsokullu@gmail.com>
2020-04-07 19:17:45 +0300

    move to process variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbUrl: 'mongodb://admin:1234qwer@ds161148.mlab.com:61148/dota',
-  telegramToken: '1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E',
-  telegramChatId: '-479108586'
+  dbUrl: process.env.dbUrl || 'mongodb://admin:1234qwer@ds161148.mlab.com:61148/dota',
+  telegramToken: process.env.telegramToken || '1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E',
+  telegramChatId: process.env.telegramChatId || '-479108586'
 }
