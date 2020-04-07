ffc08d5724a96e35cf7a17f15e36203482308f44
Harun Sokullu <harunsokullu@gmail.com>
2020-04-07 19:05:14 +0300

    remove worker http connection

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 // Configuring the database
 const config = require('./config');
 const mongoose = require('mongoose');
-var http = require("http");
 const workerController = require('./app/controllers/worker.js');
 mongoose.Promise = global.Promise;
 
@@ -14,14 +13,5 @@ mongoose.connect(config.dbUrl, {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
-
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(3001);
-
-// Console will print the message
-console.log('Server is listening on port 3001');
 
 workerController.withdraw();
