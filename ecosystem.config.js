eb3a144fb14a3add18310a0410302f510624c6a2
Harun Sokullu <harunsokullu@gmail.com>
2020-04-24 03:03:55 +0300

    update development whitelist

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     time: true,
     env: {
       NODE_ENV: "development",
-      CORS_WHITELIST: "http://localhost",
+      CORS_WHITELIST: "http://localhost:8080",
       DB_URL: "mongodb://admin:1234qwer@ds161148.mlab.com:61148/dota",
       TELEGRAM_TOKEN: "1271936615:AAH-ZQajPguxAMBp2j6vvLZfe0vN7xeoo1E",
       TELEGRAM_CHAT_ID: "-367623724"
