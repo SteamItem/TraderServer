273f4f71aaf1465bb7ee24faacf6439324586d1e
Harun Sokullu <harunsokullu@gmail.com>
2020-05-06 23:41:34 +0300

    remove unused bots

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,29 +10,5 @@ module.exports = {
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
-  }, {
-    name: "instant",
-    script: "./dist/instantWorker.js",
-    time: true,
-    env: {
-      NODE_ENV: "production",
-      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
-    }
-  }, {
-    name: "dota",
-    script: "./dist/dotaWorker.js",
-    time: true,
-    env: {
-      NODE_ENV: "production",
-      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
-    }
-  }, {
-    name: "rollbit",
-    script: "./dist/rollbitWorker.js",
-    time: true,
-    env: {
-      NODE_ENV: "production",
-      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
-    }
   }]
 }
