8ba0d6c4e24592841d02d1c9737dde762c6c2198
Harun Sokullu <harunsokullu@gmail.com>
2020-05-06 23:22:08 +0300

    update rollbit api

--- src/controllers/api/rollbit.ts
+++ src/controllers/api/rollbit.ts
@@ -7,26 +7,34 @@ export class RollbitApi {
   public async csgoInventory(cookie: string, minPrice: number, maxPrice: number) {
     var content: AxiosRequestConfig = {
       headers: {
-        'Content-Type': 'application/json',
         'Cookie': cookie,
         'Host': 'api.rollbit.com',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
       }
     };
 
-    var items = await axios.get<IRollbitInventoryItems>(`${this.baseUrl}/market?query&order=1&showTradelocked=false&showCustomPriced=false&min=${minPrice}&max=${maxPrice}`, content);
+    var url = `${this.baseUrl}/market?query&order=1&showTradelocked=false&showCustomPriced=false&min=${minPrice}&max=${maxPrice}`;
+    var items = await axios.get<IRollbitInventoryItems>(url, content);
     return items.data;
   }
 
   public async withdraw(cookie: string, refs: string[]) {
     var content: AxiosRequestConfig = {
       headers: {
-        'Content-Type': 'application/json',
+        'Accept': 'application/json, text/*',
+        'Accept-Language': 'en-US,en;q=0.9,tr;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json; charset=UTF-8',
         'Cookie': cookie,
         'Host': 'api.rollbit.com',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+        'Origin': 'https://www.rollbit.com',
+        'Pragma': 'no-cache',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
       },
-      withCredentials: true,
       timeout: 20000,
       maxRedirects: 4
     };
