f94b6990b65cf16d57dff2b834b17a19fc79b8bb
Harun Sokullu <harunsokullu@gmail.com>
2020-05-08 18:47:52 +0300

    manage bot via pm2 api

--- src/controllers/botParam.ts
+++ src/controllers/botParam.ts
@@ -1,3 +1,4 @@
+import pm2 = require('pm2');
 import BotParam = require('../models/botParam');
 import { EnumBot } from '../helpers/enum';
 
@@ -11,7 +12,57 @@ async function update(id: number, worker: boolean, code: string, cookie: string)
   if(!cookie) {
     throw new Error("cookie can not be empty");
   }
-  return BotParam.default.findOneAndUpdate({ id }, { worker, code, cookie }, {new: true});
+  return BotParam.default.findOneAndUpdate({ id }, { worker, code, cookie });
+}
+
+function manageBoot(id: EnumBot, worker: boolean) {
+  if (worker) {
+    startBot(id);
+  } else {
+    stopBot(id);
+  }
+}
+
+function getBotFileName(id: EnumBot) {
+  switch (id) {
+    case EnumBot.EmpireInstant: return "instantWorker";
+    case EnumBot.EmpireDota: return "dotaWorker";
+    case EnumBot.RollbitCsGo: return "rollbitWorker";
+    default: throw new Error("Bot not found");
+  }
+}
+
+function getWorkerPath(fileName: string) {
+  return `./dist/${fileName}.js`;
+}
+
+async function stopBot(id: number) {
+  var botFileName = this.getBotFileName(id);
+  pm2.stop(botFileName, function(err) {
+    pm2.disconnect();
+    if (err) throw err
+  });
+}
+
+async function startBot(id: number) {
+  var botFileName = getBotFileName(id);
+  var workerPath = getWorkerPath(botFileName);
+  pm2.connect(err => {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
+    pm2.start({
+      script: workerPath,
+      name: botFileName,
+      env: {
+        DB_URL: process.env.DB_URL
+      }
+    }, function(err) {
+      pm2.disconnect();
+      if (err) throw err
+    });
+  });
 }
 
 export = {
