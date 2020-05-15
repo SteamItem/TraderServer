bc4585a7561d8a712ab64fff557a6d7b78ad7ae0
Harun Sokullu <harunsokullu@gmail.com>
2020-05-15 20:51:08 +0300

    update rollbit logger config

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -40,5 +40,15 @@ module.exports = {
       TELEGRAM_TOKEN: "1271936615:AAH-ZQajPguxAMBp2j6vvLZfe0vN7xeoo1E",
       TELEGRAM_CHAT_ID: "-367623724"
     }
+  }, {
+    name: "rollbitLogger",
+    script: "./dist/rollbitLogger.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      TELEGRAM_TOKEN: "1271936615:AAH-ZQajPguxAMBp2j6vvLZfe0vN7xeoo1E",
+      TELEGRAM_CHAT_ID: "-367623724"
+    }
   }]
 }
