b99f70162037c20f62c95679c91f07a2555d8cee
Harun Sokullu <harunsokullu@gmail.com>
2020-05-16 15:10:19 +0300

    mongo useUnifiedTopology

--- src/helpers/mongo.ts
+++ src/helpers/mongo.ts
@@ -4,7 +4,8 @@ import config = require('../config');
 function connect() {
   mongoose.Promise = global.Promise;
   mongoose.connect(config.DB_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   }).then(() => {
     console.log("Successfully connected to the database");
   }).catch(err => {
