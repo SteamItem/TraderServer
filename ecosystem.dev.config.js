5127a905bae7092fd6cb252802e2e9570cfadecd
Harun Sokullu <harunsokullu@gmail.com>
2020-05-17 22:39:00 +0300

    update telegram config

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -37,8 +37,8 @@ module.exports = {
     env: {
       NODE_ENV: "production",
       DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
-      TELEGRAM_TOKEN: "1271936615:AAH-ZQajPguxAMBp2j6vvLZfe0vN7xeoo1E",
-      TELEGRAM_CHAT_ID: "-367623724"
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
     }
   }, {
     name: "rollbitLogger",
@@ -47,8 +47,8 @@ module.exports = {
     env: {
       NODE_ENV: "production",
       DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
-      TELEGRAM_TOKEN: "1271936615:AAH-ZQajPguxAMBp2j6vvLZfe0vN7xeoo1E",
-      TELEGRAM_CHAT_ID: "-367623724"
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
     }
   }]
 }
