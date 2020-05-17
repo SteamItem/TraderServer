8938b7b13aa988a6b4a58dbe2b7af76ca787c618
Harun Sokullu <harunsokullu@gmail.com>
2020-05-17 22:50:57 +0300

    update agent

--- src/controllers/api/rollbit.ts
+++ src/controllers/api/rollbit.ts
@@ -34,7 +34,7 @@ export class RollbitApi extends ApiBase {
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
       },
       timeout: 20000,
       maxRedirects: 4
