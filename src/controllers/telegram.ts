5371a7604ebca34647f3c4402241d49eb037f684
Harun Sokullu <harunsokullu@gmail.com>
2020-05-17 22:46:23 +0300

    remove telegram polling

--- src/controllers/telegram.ts
+++ src/controllers/telegram.ts
@@ -1,6 +1,6 @@
 import TelegramBot = require('node-telegram-bot-api');
 import config = require('../config');
-const bot = new TelegramBot(config.TELEGRAM_TOKEN, {polling: true});
+const bot = new TelegramBot(config.TELEGRAM_TOKEN);
 
 async function sendMessage(message: string) {
   return bot.sendMessage(config.TELEGRAM_CHAT_ID, message);
