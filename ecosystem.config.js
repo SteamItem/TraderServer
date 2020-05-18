97aad68654af0372e67909cc493015385ffbc251
Harun Sokullu <harunsokullu@gmail.com>
2020-05-19 00:37:47 +0300

    rollbit to server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,5 +20,15 @@ module.exports = {
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
+  }, {
+    name: "rollbitWorker",
+    script: "./dist/rollbitWorker.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
   }]
 }
