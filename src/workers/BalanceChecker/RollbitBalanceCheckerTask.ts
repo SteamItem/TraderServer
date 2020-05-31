5aa0d3ab481ebaba65e79ed2be5792570cfec718
Harun Sokullu <harunsokullu@gmail.com>
2020-05-31 23:16:35 +0300

    remove unnecessary codes

--- src/workers/BalanceChecker/RollbitBalanceCheckerTask.ts
+++ src/workers/BalanceChecker/RollbitBalanceCheckerTask.ts
@@ -1,7 +1,6 @@
 import { BalanceCheckerTask } from './BalanceCheckerTask';
 export class RollbitBalanceCheckerTask extends BalanceCheckerTask {
   getBalance() {
-    // TODO: Workaround fix
     return Promise.resolve(10000000);
   }
 }
