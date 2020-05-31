4aba0462dcd8a26a0da9f1c2edf1c8f804cbd7be
Harun Sokullu <harunsokullu@gmail.com>
2020-06-01 01:12:33 +0300

    update internal puppet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       CORS_WHITELIST: "https://traderbot.netlify.app",
       DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
       RDB_URL: "postgres://admin:1234qwer@srv-captain--trader-pg-db:5432/Trader?sslmode=disable",
-      PUPPET_API: "https://puppet.app.trader-bot.pw",
+      PUPPET_API: "http://srv-captain--puppet",
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
