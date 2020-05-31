2bb79a8b92041deb7b1a482190dae901308912a4
Harun Sokullu <harunsokullu@gmail.com>
2020-05-31 21:54:48 +0300

    update eco files

--- ecosystem.config.backup.js
+++ ecosystem.config.backup.js
@@ -11,5 +11,46 @@ module.exports = {
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
+  }, {
+    name: "instantWorker",
+    script: "./dist/instantWorker.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
+  }, {
+    name: "dotaWorker",
+    script: "./dist/dotaWorker.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
+  }, {
+    name: "rollbitWorker",
+    script: "./dist/rollbitWorker.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
+  }, {
+    name: "rollbitLogger",
+    script: "./dist/rollbitLogger.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb://admin:1234qwer@ds159963.mlab.com:59963/csgobot-prod",
+      RDB_URL: "postgres://admin:1234qwer@srv-captain--trader-pg-db:5432/Trader?sslmode=disable",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
   }]
 }
