9adc13c301d1b82582dc3df0a490b57e634238d4
Harun Sokullu <harunsokullu@gmail.com>
2020-06-11 21:36:50 +0300

    wait 300

--- src/workers/Worker/RollbitCsGoWorker.ts
+++ src/workers/Worker/RollbitCsGoWorker.ts
@@ -24,7 +24,7 @@ export class RollbitCsGoWorker extends RollbitWorkerBase {
 
       // const afterFilterDate = new Date();
 
-      await helpers.sleep(200);
+      await helpers.sleep(300);
       const withdrawMaker = new RollbitWithdrawMakerTask(this.botParam, inventoryFilterer.itemsToBuy, this.logger);
       currentTask = withdrawMaker.taskName;
       await withdrawMaker.work();
