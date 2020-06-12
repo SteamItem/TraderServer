e6a550c37d3be903e45efd6e753863358097240f
Harun Sokullu <harunsokullu@gmail.com>
2020-06-12 14:23:18 +0300

    try fetch

--- src/api/rollbit.ts
+++ src/api/rollbit.ts
@@ -47,4 +47,32 @@ export class RollbitApi extends ApiBase {
     const result = await axios.post(`${this.baseUrl}/withdraw`, data, content);
     return result.data;
   }
+
+  public async withdrawFetch(cookie: string, refs: string[]) {
+    const url = `${this.baseUrl}/withdraw`;
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Accept': 'application/json, text/*',
+        'Accept-Language': 'en-US,en;q=0.9,tr;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Cookie': cookie,
+        'Host': 'api.rollbit.com',
+        'Origin': 'https://www.rollbit.com',
+        'Pragma': 'no-cache',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': Constants.RollbitUserAgent
+      },
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({ "refs": refs })
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 }
