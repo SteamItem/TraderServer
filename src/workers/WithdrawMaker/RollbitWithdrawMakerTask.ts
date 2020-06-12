d1340adf2cffcc5862018b0fcf90891b7e8f80a9
Harun Sokullu <harunsokullu@gmail.com>
2020-06-12 21:04:58 +0300

    promise any

--- src/workers/WithdrawMaker/RollbitWithdrawMakerTask.ts
+++ src/workers/WithdrawMaker/RollbitWithdrawMakerTask.ts
@@ -1,4 +1,4 @@
-// import promiseAny = require('promise-any');
+import promiseAny = require('promise-any');
 import { IBotParam } from '../../models/botParam';
 import { IRollbitInventoryItem } from '../../interfaces/rollbit';
 import { WithdrawMakerTask } from './WithdrawMakerTask';
@@ -20,13 +20,12 @@ export class RollbitWithdrawMakerTask<II extends IRollbitInventoryItem> extends 
     const itemName = ib.items.map(ii => ii.name).join("#");
     try {
       const api = new RollbitApi();
-      // const p1 = api.withdraw(this.botParam.cookie, [ib.ref]);
-      // const p2 = api.withdraw(this.botParam.cookie, [ib.ref]);
-      // const p3 = api.withdraw(this.botParam.cookie, [ib.ref]);
+      const p1 = api.withdraw(this.botParam.cookie, [ib.ref]);
+      const p2 = api.withdraw(this.botParam.cookie, [ib.ref]);
+      const p3 = api.withdraw(this.botParam.cookie, [ib.ref]);
 
-      // const promises = [p1, p2, p3]
-      // await promiseAny(promises);
-      await api.withdraw(this.botParam.cookie, [ib.ref]);
+      const promises = [p1, p2, p3]
+      await promiseAny(promises);
 
       this.successWithdrawResult.push({name: itemName, price: ib.price});
     } catch (e) {
