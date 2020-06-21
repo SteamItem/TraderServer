1db666de3ad1adb25d5b419c5f035bb6de619399
Harun Sokullu <harunsokullu@gmail.com>
2020-06-21 11:15:36 +0300

    merge save

--- src/controllers/wishlistItem.ts
+++ src/controllers/wishlistItem.ts
@@ -8,7 +8,15 @@ async function findAll(): Promise<IWishlistItem[]> {
     return await WishlistItem.find({}).exec();
 }
 
-async function create(site_id: number, appid: number, name: string, max_price: number): Promise<IWishlistItem> {
+function create(site_id: number, appid: number, name: string, max_price: number): Promise<IWishlistItem> {
+    return save(site_id, appid, name, max_price);
+}
+
+function update(itemId: string, site_id: number, appid: number, name: string, max_price: number): Promise<IWishlistItem> {
+    return save(site_id, appid, name, max_price);
+}
+
+async function save(site_id: number, appid: number, name: string, max_price: number): Promise<IWishlistItem> {
     if(!site_id) {
         throw new Error("site_id can not be empty");
     }
@@ -24,22 +32,6 @@ async function create(site_id: number, appid: number, name: string, max_price: n
     return await wishlistItem.save();
 }
 
-async function update(itemId: string, site_id: number, appid: number, name: string, max_price: number): Promise<IWishlistItem> {
-    if(!itemId) {
-        throw new Error("itemId can not be empty");
-    }
-    if(!site_id) {
-        throw new Error("site_id can not be empty");
-    }
-    if(!appid) {
-        throw new Error("appid can not be empty");
-    }
-    if(!name) {
-        throw new Error("name can not be empty");
-    }
-    await WishlistItem.findOneAndDelete({ site_id, appid, name }).exec();
-    return await WishlistItem.findByIdAndUpdate(itemId, { site_id, appid, name, max_price }, {new: true}).exec();
-}
 
 async function remove(itemId: string): Promise<IWishlistItem> {
     return await WishlistItem.findByIdAndRemove(itemId).exec();
