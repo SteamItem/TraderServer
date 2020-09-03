7c247e34e28ca5a2eb15ed093460cb6b7a74217f
Harun Sokullu <harunsokullu@gmail.com>
2020-09-03 22:51:29 +0300

    update eco

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   apps : [{
-    name: "server",
-    script: "./dist/src/server.js",
+    name: "rollbitWorker",
+    script: "./dist/src/rollbitWorker.js",
     time: true,
     env: {
       NODE_ENV: "production",
-      CORS_WHITELIST: "https://traderbot.netlify.app",
       DB_URL: "mongodb+srv://admin:1234qwer@mlab.cifvs.mongodb.net/csgobot-prod?retryWrites=true&w=majority",
-      RDB_URL: "postgres://admin:1234qwer@srv-captain--trader-pg-db:5432/Trader?sslmode=disable",
-      PUPPET_API: "http://srv-captain--puppet",
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
