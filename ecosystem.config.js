f8982a1843c507456cb6e36ae56e6ec40bb8e338
Harun Sokullu <harunsokullu@gmail.com>
2020-09-03 21:09:17 +0300

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,5 +12,15 @@ module.exports = {
       TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
       TELEGRAM_CHAT_ID: "-479108586"
     }
+  }, {
+    name: "rollbitWorker",
+    script: "./dist/src/rollbitWorker.js",
+    time: true,
+    env: {
+      NODE_ENV: "production",
+      DB_URL: "mongodb+srv://admin:1234qwer@mlab.cifvs.mongodb.net/csgobot-prod?retryWrites=true&w=majority",
+      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
+      TELEGRAM_CHAT_ID: "-479108586"
+    }
   }]
 }
