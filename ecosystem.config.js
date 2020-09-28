daea54aa5ee9d4bb356635ad52b0da63f2d88761
Harun Sokullu <harunsokullu@gmail.com>
2020-09-28 21:04:49 +0300

    update eco remove args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,6 @@ module.exports = {
   apps : [{
     name: "server",
     script: "./dist/src/server.js",
-    time: true,
-    env: {
-      NODE_ENV: "production",
-      CORS_WHITELIST: "https://traderbot.netlify.app",
-      DB_URL: "mongodb+srv://admin:1234qwer@mlab.cifvs.mongodb.net/csgobot-prod?retryWrites=true&w=majority",
-      RDB_URL: "postgres://admin:1234qwer@srv-captain--trader-pg-db:5432/Trader?sslmode=disable",
-      PUPPET_API: "http://srv-captain--puppet",
-      TELEGRAM_TOKEN: "1274822023:AAHdhNgE194hHQJRaqw-EIIf-fn4VB1ZN4E",
-      TELEGRAM_CHAT_ID: "-479108586"
-    }
+    time: true
   }]
 }
